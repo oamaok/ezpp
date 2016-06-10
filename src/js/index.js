cd97c1aa88deeb3945032299ee696fd6d85d03f5
Teemu Pääkkönen <oamaok@gmail.com>
2016-06-10 16:45:01 +0300

    Add support for new.ppy.sh

--- src/js/index.js
+++ src/js/index.js
@@ -32,29 +32,38 @@ chrome.tabs.query({
     lastFocusedWindow: true // In the current window
 }, tabs => {
   const url = tabs[0].url;
-  const match = url.toLowerCase().match(/^https?:\/\/(osu|new).ppy.sh\/([bs])\/(\d+)/);
+  const match = url.toLowerCase().match(/^https?:\/\/(osu|new).ppy.sh\/([bs])\/(\d+)#?(\d+)?/);
   pageInfo.isOldSite = match[1] == 'osu';
   pageInfo.isBeatmap = match[2] == 'b';
   const id = match[3];
 
-  // Fetch the current page for data not found in the url.
-  fetch(url)
-  .then(res => res.text())
-  .then(html => {
-    pageInfo.beatmapSetId = pageInfo.isBeatmap
-      ? html.match(/beatmap-rating-graph\.php\?s=(\d+)/)[1]
-      : id;
+  let promise = null;
 
-    pageInfo.beatmapId = pageInfo.isBeatmap
-      ? id
-      : html.match(/class='beatmapTab active' href='\/b\/(\d+)/)[1];
+  if (pageInfo.isOldSite) {
+    // For the old (current) version of the site ID values must be found from the page source.
+    promise = fetch(url)
+    .then(res => res.text())
+    .then(html => {
+      pageInfo.beatmapSetId = pageInfo.isBeatmap
+        ? html.match(/beatmap-rating-graph\.php\?s=(\d+)/)[1]
+        : id;
 
-    // Check for 'Updated' text instead of 'Qualified' or 'Ranked'
-    pageInfo.isUnranked = !!html.match('<td width=0%>\nSubmitted:<br/>\nUpdated:\n</td>');
+      pageInfo.beatmapId = pageInfo.isBeatmap
+        ? id
+        : html.match(/class='beatmapTab active' href='\/b\/(\d+)/)[1];
 
-    return fetch(`https://osu.ppy.sh/osu/${pageInfo.beatmapId}`);
-  })
-  .then(res => res.text())
+      // Check for 'Updated' text instead of 'Qualified' or 'Ranked'
+      pageInfo.isUnranked = !!html.match('<td width=0%>\nSubmitted:<br/>\nUpdated:\n</td>');
+
+      return fetch(`https://osu.ppy.sh/osu/${pageInfo.beatmapId}`);
+    });
+  } else {
+    pageInfo.beatmapSetId = match[3];
+    pageInfo.beatmapId = match[4];
+    promise = fetch(`https://osu.ppy.sh/osu/${pageInfo.beatmapId}`);
+  }
+
+  promise.then(res => res.text())
   .then(OsuParser.parseContent)
   .then(beatmap => {
     cleanBeatmap = beatmap;
@@ -86,12 +95,6 @@ const onReady = (cover) => {
   // Set header text
   titleElement.innerText = `${cleanBeatmap.Artist} - ${cleanBeatmap.Title} [${cleanBeatmap.Version}]`;
 
-  document.addEventListener('keydown', evt => {
-    // Check for enter
-    if (evt.keyCode == 13)
-      calculate();
-  });
-
   // Recalculate the result div if the form is fiddled with
   Array.from(modifierElements).forEach(
     modElement => modElement.addEventListener('click', debounce)
@@ -109,7 +112,7 @@ const debounce = () => {
   if (debounceTimeout)
     clearTimeout(debounceTimeout);
 
-  debounceTimeout = setTimeout(calculate, 300);
+  debounceTimeout = setTimeout(calculate, 500);
 };
 
 const calculate = () => {
