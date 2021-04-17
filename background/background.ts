82cfba79676a08de7ae081e3e965e8547862014d
acrylic-style <ilyy@outlook.jp>
2021-04-17 13:29:27 +0900

    Fix throwing error on discussion page

--- background/background.ts
+++ background/background.ts
@@ -1,9 +1,16 @@
 import { BEATMAP_URL_REGEX } from '../common/constants'
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.url!.match(BEATMAP_URL_REGEX)) {
+  if (
+    changeInfo.status === 'complete' &&
+    tab.url!.match(BEATMAP_URL_REGEX) &&
+    !tab.url!.includes('/discussion')
+  ) {
     chrome.action.enable(tabId)
-  } else if (!tab.url!.match(BEATMAP_URL_REGEX)) {
+  } else if (
+    !tab.url!.match(BEATMAP_URL_REGEX) ||
+    tab.url!.includes('/discussion')
+  ) {
     chrome.action.disable(tabId)
   }
 })
